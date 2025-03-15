5dbccfc50a8c7e7b3254417e344717d186175290
kaedenpeterson <146796640+kaedenpeterson@users.noreply.github.com>
2025-03-15 01:57:59 -0700

    Fixed file name typo

--- ClassScheduler/obj/Debug/net9.0/ClassScheduler.AssemblyInfo.cs
+++ ClassScheduler/obj/Debug/net9.0/ClassScheduler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassScheduler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e4f41f3a24d4065f7467dbc0e51a6e81550014a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1cf83a5fdf6c8d78529ca6a8773214939934cf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassScheduler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassScheduler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
